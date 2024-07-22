efd50e70a69b5a174d3204c233a65c5ced597f4a
Elizabeth Proctor <elizabethproctor66@gmail.com>
2024-07-21 17:10:30 -0700

    created student and advisor classes

---
+++ NestedObjects/Advisor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represents a college advisor
+    /// </summary>
+    public class Advisor
+    {
+        /// <summary>
+        /// Legal First and Last Name.
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Employee Email.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Office Location (Building, Room Number)
+        /// </summary>
+        public string OfficeLocation { get; set; }
+
+    }
+}
---
+++ NestedObjects/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represents a individual college student. 
+    /// </summary>
+    public class Student
+    {
+        /// <summary>
+        /// Legal First Name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Legal Last Name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The assigned school email address.
+        /// </summary>
+        public string SchoolEmail { get; set; }
+
+        /// <summary>
+        /// Phone Number
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+        public Advisor AssignedAdvisor { get; set; }
+    }
+}
